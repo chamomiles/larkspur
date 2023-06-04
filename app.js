e1756ba0bb67dabdeb70fe6a6ced34321649d7f1
Chamomile <tima.chamomile@gmail.com>
2023-06-04 12:55:31 +0300

    Revert to JS

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+
+// Load config
+dotenv.config({ path: './config/config.env'})
+
+// Initialize app
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
